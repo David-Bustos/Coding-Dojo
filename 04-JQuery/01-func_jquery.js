7a49489c0d2ec053877fa799ae1c0c8d0132f261
David Andrés Bustos Oñate <david.bustosonate@gmail.com>
2021-05-03 12:59:54 -0400

    refactoring js

--- 04-JQuery/01-func_jquery.js
+++ 04-JQuery/01-func_jquery.js
@@ -1,49 +1,18 @@
-// $(document).ready(function () {
-//     $('img').hide();
+$(document).ready(function(){
 
-//     $('.Class button').click(function () {
-//         $('.Class p').addClass("red");
-//         console.log("we changed the class");
-//     });
-//     $('.Slide button').click(function(){
-//         $('.Slide img').toggle("slide");
-//     });
-//     $('.Append button').click(function(){
-//         $('.Append h3').append($("<p> We appended this p! </p>"));  
-//     });
-//     $('.SlideByParts button').click(function () {
-//         if ($('.SlideByParts img').is(":hidden")) {
-//             $('.SlideByParts img').slideDown("slow");
-//             $('.SlideByParts button').text("slide up!");
-//         } else {
-//             $('.SlideByParts img').slideUp("slow");
-//             $('.SlideByParts button').text("slide down!").fadeIn();
-//         }
-//     });
+    // Hide - Oculta el contenido
+    $('#hide').on('click', function(){
+        $(this).parent().siblings().hide();
+    })
 
-//     $('.Fade button').click(function () {
-//         if ($('.Fade p').is(":hidden")) {
-//             $('.Fade p').fadeIn("slow");
-//             $('.Fade button').text("fade out!")
-//         } else {
-//             $('.Fade p').fadeOut("slow");
-//             $('.Fade button').text("fade in!")
-//         }
-//     });
-//     $('.Insert button').click(function () {
-//         $('.Insert p').before("<h1>B</h1>");
-//         $('.Insert p').after("<h1>A</h1>");
-//     });
-//     $('.ShowHTML button').click(function () {
-//         $('.ShowHTML p').html("<span>I replaced the p with new HTML</span>");
-//     });
+    // Show - Muestra el contenido
+    $('#show').on('click', function(){
+        $(this).parent().siblings().show();
+    })
 
-//     $('.Valuefunc input').keyup(function () {
-//         var value = $(this).val();
-//         $('.Valuefunc p').text(value);
-//     });
-//     $('.Attribute button').click(function () {
-//         $('img').attr({src:"images/python.png"});
-//     });
+    // Toogle - Oculta y Muestra el contenido
+    $('#toggle').on('click', function(){
+        $(this).parent().siblings().toggle();
+    })
 
-// });
+});
