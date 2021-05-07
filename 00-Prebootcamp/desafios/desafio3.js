7068dd738c2e0e89aba98fc497837dc712a17b19
David Bustos <david.bustosonate@gmail.com>
2021-05-06 20:49:09 -0400

    Delete 00-Prebootcamp directory

--- 00-Prebootcamp/desafios/desafio3.js
+++
@@ -1,8 +0,0 @@
-function printSumArray(x) {
-    var sum = 0;
-    for (var i = 0; i < x.length; i++) {
-        sum += x[i]
-    }
-    return sum;
-}
-console.log( printSumArray([1,2,3]) ); // debe imprimir 6
